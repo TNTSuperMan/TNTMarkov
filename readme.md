3c0cee02172cb17baba6316d930980bd0fd9c3bb
TNTSuperMan <tntsuperga@gmail.com>
2024-08-08 21:57:32 +0900

    model

--- readme.md
+++ readme.md
@@ -3,6 +3,4 @@ markovifyが上手く動作しないため自作しました。
 # 使い方
 GithubのWikiにて。
 # サンプルについて
-model.txtは、[e-gov法令検索](https://elaws.e-gov.go.jp/)の刑法第二編第八章からの引用です。  
-また、sample.pyは崩壊した法律メーカーであり、model.txtを使っています。  
-また、このサンプルの動作にはMeCabの導入が必要です。
+model.txtは、青空文庫からの吾輩は猫であるです。
--- sample.py
+++ sample.py
@@ -2,15 +2,13 @@ import MeCab
 import tntmarkov as mkv
 
 def main():
-    print("TNTMarkov サンプルプログラム")
-    print("~ Broken Law Generator ~\n")
     with open("model.txt", "r", encoding="utf-8") as f:
         text = f.read()
     mecab = MeCab.Tagger("-Owakati")
     words = mecab.parse(text)
     model = mkv.MakeModel(words.split(" "))
     while(input() == ""):
-        print(mkv.MekeMarkovText(model, "第", "。"))
+        print(mkv.MekeMarkovText(model, first="吾輩", end="。"))
 
 
 if(__name__ == "__main__"):
